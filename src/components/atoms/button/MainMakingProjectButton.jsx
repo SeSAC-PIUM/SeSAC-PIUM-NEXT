39db47e9fd594491f722424990a90f40797226cf
YangHK95 <ekzm7040@naver.com>
2023-09-25 17:37:42 +0900

    RadomMatching&MakingProject

---
+++ src/components/atoms/button/MainMakingProjectButton.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import BannerNavLink from "../link/BannerNavLink";
+
+export default function MainMakingProjectButton() {
+  return (
+    <>
+      <BannerNavLink text1="아이디가 있다면" text2="프로젝트 생성✨" href="#" />
+    </>
+  );
+}
