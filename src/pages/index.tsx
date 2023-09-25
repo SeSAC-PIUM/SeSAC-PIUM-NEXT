8291530b6500dd4766f6d92e69fdb363510a765d
ymj0828 <ymj369@nate.com>
2023-09-25 23:37:47 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,5 +4,6 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>메인 작업 페이지 입니다.</main>;
+  // ctrl + i - tailwind 자동완성 단축키
+  return <main className="border">메인 작업 페이지 입니다.</main>;
 }
