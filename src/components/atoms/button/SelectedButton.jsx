786895fa768119ad45f0d7c8c984cc3168a9271c
ymj0828 <ymj369@nate.com>
2023-09-30 23:18:40 +0900

    button style apply

--- src/components/atoms/button/SelectedButton.jsx
+++ src/components/atoms/button/SelectedButton.jsx
@@ -1,9 +1,8 @@
 export default function SelectedButton(props) {
-  // console.log(props, 'Qhd');
   const {item} = props
   return (
     <li
-      className={`border-[1px] border-[green] rounded w-fit py-2 px-4`}
+      className={`selected_button`}
     >
       {item}
     </li>
