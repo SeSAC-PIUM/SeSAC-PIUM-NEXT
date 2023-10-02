73c4a31b0df7f4b280ad6e22017d3edfbcbc7af7
Dayglow0926 <dayglow0926@gmail.com>
2023-10-02 23:46:54 +0900

    move: firebase init 파일 이동

--- src/firebase/firebaseApp.ts
+++ src/firebase/firebaseApp.ts
@@ -1,6 +1,12 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA7PrbQP-mR93Zo8BssWuxcxsSjxzw8sH4",
   authDomain: "sesac-pium.firebaseapp.com",
@@ -13,4 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export default app;
