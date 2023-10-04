de1a2c3d4e00c031402e6ec990aff28ec0cba545
ymj0828 <ymj369@nate.com>
2023-10-04 10:48:43 +0900

    create_project 수정

--- src/components/atoms/button/DefaultButton.jsx
+++ src/components/atoms/button/DefaultButton.jsx
@@ -2,7 +2,7 @@ export default function DefaultButton(props) {
   const {item} = props
   return (
     <li
-      className={'default_button'}
+      className={'border-[1px] border-[#e9e9ee] bg-white rounded w-fit py-2 px-4 cursor-pointer'}
     >
       {item}
     </li>
