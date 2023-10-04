42bff97661a3c060f5c03f2f9f3e8a4f1e82118f
lkf6214 <lkf6214@naver.com>
2023-10-04 10:45:38 +0900

    del: DefaultButton

--- src/components/molecule/SignUpBox.tsx
+++ src/components/molecule/SignUpBox.tsx
@@ -4,11 +4,11 @@ import DefaultButton from "../atoms/button/DefaultButton";
 export default function DefaultCard() {
   return (
     <div className="border rounded-[4px] ring-0 ring-[#E9E9EE] p-[16px]  bg-white ">
-      <DefaultButton text="기획" />
-      <DefaultButton text="UX/XI 디자인" />
-      <DefaultButton text="퍼블리싱" />
-      <DefaultButton text="프론트엔드" />
-      <DefaultButton text="백엔드" />
+      <DefaultButton item="기획" />
+      <DefaultButton item="UX/XI 디자인" />
+      <DefaultButton item="퍼블리싱" />
+      <DefaultButton item="프론트엔드" />
+      <DefaultButton item="백엔드" />
     </div>
   );
 }
