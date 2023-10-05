a8656d349eb002791867f26ca73d47d8028e2cd4
Dayglow0926 <dayglow0926@gmail.com>
2023-10-05 18:48:36 +0900

    feat: useForm 연결을 위한 register 추가

--- src/components/atoms/input/SignUpInput.tsx
+++ src/components/atoms/input/SignUpInput.tsx
@@ -1,14 +1,22 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
 interface SignUpInputProps {
+  register: UseFormRegisterReturn;
   placeholder?: string;
+  type?: string;
 }
 
-export default function SignUpInput({ placeholder }: SignUpInputProps) {
+export default function SignUpInput({
+  register,
+  placeholder,
+  type = "text",
+}: SignUpInputProps) {
   return (
     <>
       <input
+        {...register}
         className="focus:ring-2 focus:ring-[#2DA96E] focus:outline-none appearance-none  leading-8 text-slate-700 placeholder-[#9090A0] rounded py-2 px-8 ring-1 ring-[#E9E9EE] shadow-sm "
-
-        type="text"
+        type={type}
         aria-label="Filter projects"
         placeholder={placeholder}
       />
