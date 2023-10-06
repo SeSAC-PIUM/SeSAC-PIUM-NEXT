eafb90aa542a0f2b6b19844fbcbb35040c1cb073
Dayglow0926 <dayglow0926@gmail.com>
2023-10-06 18:50:40 +0900

    feat: 로그인 유지 기능 useRef 연결

--- src/components/atoms/button/CheckBoxButton.tsx
+++ src/components/atoms/button/CheckBoxButton.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { MutableRefObject } from "react";
 interface CheckBoxButtonProps {
   label: string;
   name: string;
   value: string;
+  autoLoginRef: MutableRefObject<null>;
 }
 
 export default function RadioButton({
   label,
   name,
   value,
+  autoLoginRef,
 }: CheckBoxButtonProps): React.ReactElement {
   return (
     <>
       <label className="h-full p-0">
         <input
+          ref={autoLoginRef}
           type="checkbox"
           name={name}
           value={value}
