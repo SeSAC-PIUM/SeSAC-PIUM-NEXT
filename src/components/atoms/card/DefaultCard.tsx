06ddc9359b7d28407a50ef781d6f0c5adb7aa52b
lkf6214 <lkf6214@naver.com>
2023-10-06 15:04:57 +0900

    충돌 input

--- src/components/atoms/card/DefaultCard.tsx
+++ src/components/atoms/card/DefaultCard.tsx
@@ -5,7 +5,7 @@ interface DefaultCardProps {
 export default function DefaultCard({ children }: DefaultCardProps) {
   return (
     <>
-      <div className="border rounded ring-[#E9E9EE] p-4  bg-white">
+      <div className="border rounded border-[#E9E9EE] p-4  bg-white">
         {children && children}
       </div>
       ;
