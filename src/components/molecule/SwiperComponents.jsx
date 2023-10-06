baaece83d2bd66f8bd8c2cff0cb8931cf3698039
YangHK95 <ekzm7040@naver.com>
2023-10-06 09:10:03 +0900

    배너

--- src/components/molecule/SwiperComponents.jsx
+++ src/components/molecule/SwiperComponents.jsx
@@ -1,19 +1,16 @@
 import Image from "next/image";
 import { useRef, useState } from "react";
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Autoplay,
-} from "swiper/modules";
+
+import { Navigation, Pagination, Autoplay, Mousewheel } from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperButton from "../atoms/button/SwiperButton";
+import "swiper/swiper-bundle.css";
 
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
+import "swiper/swiper-bundle.css";
 
 export default function SwiperComponents({ className, slideClassName, lists }) {
   const prevRef = useRef(null);
@@ -21,13 +18,20 @@ export default function SwiperComponents({ className, slideClassName, lists }) {
 
   const setting = {
     className,
-    pagination: true,
-    modules: [Pagination, Navigation, Autoplay],
-    autoplay: true,
+    pagination: {
+      type: "fraction",
+    },
+    modules: [Pagination, Navigation, Autoplay, Mousewheel],
+    autoplay: {
+      dispay: 3000,
+      disableOnInteraction: false,
+    },
+    mousewheel: true,
     navigation: {
       prevEl: prevRef.current, // 이전 버튼
       nextEl: nextRef.current, // 다음 버튼
     },
+
     onBeforeInit: (swiper) => {
       // 초기 설정
       swiper.params.navigation.prevEl = prevRef.current;
@@ -38,16 +42,18 @@ export default function SwiperComponents({ className, slideClassName, lists }) {
   };
 
   return (
-    <div className="relative group w-[906px]  h-[260px] m-0 rounded-[8px]">
-      <SwiperButton position="left" buttonRef={prevRef} />
+    <div className="relative group w-[906px]  h-[260px] m-0">
       <Swiper {...setting}>
         {lists.map((v, i) => (
           <SwiperSlide className={slideClassName} key={i}>
             {v}
           </SwiperSlide>
         ))}
+        <div class="flex">
+          <SwiperButton position="left" buttonRef={prevRef} />
+          <SwiperButton position="right" buttonRef={nextRef} />
+        </div>
       </Swiper>
-      <SwiperButton position="right" buttonRef={nextRef} />
     </div>
   );
 }
