4b0265a2cd5bf03888a0bff32ecdef433d5a1d52
Dayglow0926 <dayglow0926@gmail.com>
2023-10-07 17:47:49 +0900

    feat: 회원가입 유효성 검사 추가

--- src/components/template/SignUpEmailTemplate.tsx
+++ src/components/template/SignUpEmailTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import CheckBoxButton from "../atoms/button/CheckBoxButton";
 import GreenButton from "../atoms/button/GreenButton";
 import SignUpInput from "../atoms/input/SignUpInput";
@@ -6,9 +6,46 @@ import WarningLabel from "../atoms/label/WarningLabel";
 
 import LogoName from "../../img/logo&name.svg";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+
+interface signUpForm {
+  email: string;
+  password: string;
+  passwordCheck: string;
+}
+
+const passwordRegular = {
+  num: /[0-9]/,
+  eng: /[a-zA-Z]/,
+  spc: /[~!@#$%^&*()_+|<>?:{}]/,
+};
 
 export default function SignUpTemplate({}) {
   const className = "flex flex-wrap bg-slate-100 items-start  pb- w-full";
+
+  const signAll = useRef(null);
+  const signUp1 = useRef(null);
+  const signUp2 = useRef(null);
+  const signUp3 = useRef(null);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    getValues,
+  } = useForm<signUpForm>({ mode: "onChange" });
+
+  const onValid = () => {};
+
+  const onInvalid = () => {};
+
+  useEffect(() => {
+    console.log("signAll");
+    console.log(signAll.current);
+  }, [signAll]);
+
+  // console.log(errors);
   return (
     <>
       <section className="w-[360px] m-auto pt-[60px]">
@@ -22,19 +59,64 @@ export default function SignUpTemplate({}) {
           </div>
         </div>
 
-        <div className="flex flex-col gap-4">
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={handleSubmit(onValid, onInvalid)}
+        >
           <div className="flex flex-col gap-4">
             <div>
-              <SignUpInput placeholder="이메일" />
-              <WarningLabel text="이메일 형식으로 입력해주세요" />
+              <SignUpInput
+                register={register("email", {
+                  validate: {
+                    email: (value) =>
+                      value.includes("@") || "이메일을 입력해주세요.",
+                  },
+                })}
+                type="email"
+                placeholder="이메일"
+              />
+              {errors.email && (
+                <WarningLabel text="이메일 형식으로 입력해주세요" />
+              )}
             </div>
             <div>
-              <SignUpInput placeholder="비밀번호" />
-              <WarningLabel text="비밀번호는 영문+숫자 조합 8자 이상 입력해주세요" />
+              <SignUpInput
+                register={register("password", {
+                  required: true,
+                  minLength: {
+                    message:
+                      "비밀번호는 영문+숫자 조합 8자 이상 입력해주세요.!!",
+                    value: 8,
+                  },
+                  validate: {
+                    password: (value) =>
+                      (passwordRegular.num.test(value) &&
+                        passwordRegular.eng.test(value)) ||
+                      `passwordRegular.test(value) : ${value}`,
+                  },
+                })}
+                type="password"
+                placeholder="비밀번호"
+              />
+              {errors.password && (
+                <WarningLabel text="비밀번호는 영문+숫자 조합 8자 이상 입력해주세요." />
+              )}
             </div>
             <div>
-              <SignUpInput placeholder="비밀번호 확인" />
-              <WarningLabel text="비밀번호가 일치하지 않아요" />
+              <SignUpInput
+                register={register("passwordCheck", {
+                  validate: {
+                    passwordCheck: (value) =>
+                      getValues("password") === value ||
+                      "비밀번호가 일치하지 않아요",
+                  },
+                })}
+                type="password"
+                placeholder="비밀번호 확인"
+              />
+              {errors.passwordCheck && (
+                <WarningLabel text="비밀번호가 일치하지 않아요" />
+              )}
             </div>
           </div>
 
@@ -43,6 +125,7 @@ export default function SignUpTemplate({}) {
               label="전체 이용약관 동의"
               name="signIn"
               value="signIn"
+              checkRef={signAll}
             />
             <button className="text-[--color-grayscale-500] ">자세히</button>
           </div>
@@ -52,19 +135,22 @@ export default function SignUpTemplate({}) {
               label="(필수) 개인정보 수집 이용 동의"
               name="signUp1"
               value="(필수) 개인정보 수집 이용 동의"
+              checkRef={signUp1}
             />
             <CheckBoxButton
               label="(필수) 개인정보 제3자 제공 동의"
               name="signUp2"
               value="(필수) 개인정보 제3자 제공 동의"
+              checkRef={signUp2}
             />
             <CheckBoxButton
               label="(선택) 광고성 정보수진 동의"
               name="signUp3"
               value="(선택) 광고성 정보수진 동의"
+              checkRef={signUp3}
             />
           </div>
-        </div>
+        </form>
 
         <GreenButton className=" my-6" text="가입완료" />
       </section>
