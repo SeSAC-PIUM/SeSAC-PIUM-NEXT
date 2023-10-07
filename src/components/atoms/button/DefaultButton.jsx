598f2d0ea29e6fccecbdd63f118982acbf7a63e6
lkf6214 <lkf6214@naver.com>
2023-10-07 18:16:07 +0900

    content영역 나눔

--- src/components/atoms/button/DefaultButton.jsx
+++ src/components/atoms/button/DefaultButton.jsx
@@ -1,6 +1,6 @@
 export default function DefaultButton({ children }) {
   return (
-    <button className="border-[1px] border-[--color-grayscale-200] rounded w-fit py-4 px-8 ">
+    <button className="border-[1px] border-[--color-grayscale-200] rounded w-fit py-2 px-4 ">
       {children}
     </button>
   );
--- src/components/atoms/input/DefaultInput.tsx
+++ src/components/atoms/input/DefaultInput.tsx
@@ -11,7 +11,6 @@ export default function SignUpInput({
     <div className=" relative flex  items-center ">
       <input
         className="focus:ring-2 focus:ring-[#2DA96E] focus:outline-none appearance-none  leading-8 text-slate-700 text-[14px] placeholder-[#9090A0] rounded py-2 px-8 ring-1 ring-[#E9E9EE] shadow-sm flex-1"
-
         type="text"
         aria-label="Filter projects"
         placeholder={placeholder}
