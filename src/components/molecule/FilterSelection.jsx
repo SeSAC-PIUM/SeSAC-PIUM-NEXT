bddb021de47737f5a66c008dbb29151aabc4f6c6
ymj0828 <ymj369@nate.com>
2023-10-11 10:34:26 +0900

    project list 수정

--- src/components/molecule/FilterSelection.jsx
+++ src/components/molecule/FilterSelection.jsx
@@ -1,7 +1,12 @@
-export default function FilterSelection(){
+export default function FilterSelection({text}){
+  console.log(text)
   return(
   <ul>
-    
+          {text.map((ele, index) => {
+        return (
+          <li>{ele}</li>
+        );
+      })}
   </ul>
 
   )
