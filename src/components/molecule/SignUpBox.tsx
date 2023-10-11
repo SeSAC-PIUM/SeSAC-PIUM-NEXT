358acea1d620697e703d68ed97b454da76eae8be
Dayglow0926 <dayglow0926@gmail.com>
2023-10-12 02:14:22 +0900

    fix: build 오류 수정

--- src/components/molecule/SignUpBox.tsx
+++ src/components/molecule/SignUpBox.tsx
@@ -4,11 +4,11 @@ import DefaultButton from "../atoms/button/DefaultButton";
 export default function SignUpBox() {
   return (
     <div className="0border rounded-[4px] ring-0 ring-[#E9E9EE] p-[16px]  bg-white ">
-      <DefaultButton item="기획" />
-      <DefaultButton item="UX/XI 디자인" />
-      <DefaultButton item="퍼블리싱" />
-      <DefaultButton item="프론트엔드" />
-      <DefaultButton item="백엔드" />
+      <DefaultButton>기획</DefaultButton>
+      <DefaultButton>UX/UI디자인</DefaultButton>
+      <DefaultButton>퍼블리싱</DefaultButton>
+      <DefaultButton>프론트엔드</DefaultButton>
+      <DefaultButton>백엔드</DefaultButton>
     </div>
   );
 }
