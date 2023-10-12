e257f0bf774814a74e213dcd0e4d0a51b9080ddc
lkf6214 <lkf6214@naver.com>
2023-10-13 05:15:06 +0900

    add: reservation swiper

--- src/components/template/ReservationTemplate.tsx
+++ src/components/template/ReservationTemplate.tsx
@@ -1,26 +1,25 @@
 import React from "react";
 import ReservationBox from "../organism/ReservationBox";
+import SwiperReservation from "../organism/SwiperReservation";
 import PageTitle from "../atoms/label/PageTitle";
 // import SectionTitle from "../atoms/label/SectionTitle";
 
 export default function reservationTemplate() {
   return (
     <>
-      <PageTitle
-        title1="공간예약"
-        title2=""
-        PointTitle1=""
-        PointTitle2=""
-        border="border-b-[1px]"
-      />
-      {/* <SectionTitle
-        title1=""
-        PointTitle1="강동캠퍼스"
-        title2="스터디 공간을 예약해 보세요"
-        PointTitle2=""
-        border="border-b-2"
-      /> */}
-      <ReservationBox />
+      <section className=" max-w-[800px] w-full m-auto ">
+        <PageTitle
+          title1="공간예약"
+          title2=""
+          PointTitle1=""
+          PointTitle2=""
+          border="border-b-[1px]"
+        />
+        <div className="flex flex-wrap pt-[40px]">
+          <SwiperReservation />
+          <ReservationBox />
+        </div>
+      </section>
     </>
   );
 }
