8908b22f5bfda73190eabd7f9cfac232d805287a
lkf6214 <lkf6214@naver.com>
2023-10-13 02:25:47 +0900

    add: validate 한글만 입력

--- src/components/template/SignUpBasicTemplate.tsx
+++ src/components/template/SignUpBasicTemplate.tsx
@@ -54,7 +54,13 @@ export default function SignUpTemplate({}) {
             register={register("name", {
               required: true,
               validate: {
-                name: (value) => value.length < 3 && "세글자 이상 입력하세요.",
+                // name: (value) => value.length < 3 && "세글자 이상 입력하세요.",
+                name: (value) => {
+                  const koreanRegex = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$/;
+                  return (
+                    koreanRegex.test(value) || "한글로 실명을 입력해주세요."
+                  );
+                },
               },
             })}
             errors={errors}
