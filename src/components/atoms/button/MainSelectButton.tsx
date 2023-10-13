ebbcccbeeefc3d1079ca2a6fbcdacd8b142973d6
YangHK95 <ekzm7040@naver.com>
2023-10-13 12:52:00 +0900

    a태그

--- src/components/atoms/button/MainSelectButton.tsx
+++ src/components/atoms/button/MainSelectButton.tsx
@@ -3,12 +3,30 @@ import MainSelectButtonLink from "../link/MainSelectButtonLink";
 export default function MainSelectButton() {
   return (
     <div className="flex justify-center gap-[12px]">
-      <MainSelectButtonLink text="전체보기" href="#"></MainSelectButtonLink>
-      <MainSelectButtonLink text="기획" href="#"></MainSelectButtonLink>
-      <MainSelectButtonLink text="UX/UI디자인" href="#"></MainSelectButtonLink>
-      <MainSelectButtonLink text="퍼블리싱" href="#"></MainSelectButtonLink>
-      <MainSelectButtonLink text="프론트엔드" href="#"></MainSelectButtonLink>
-      <MainSelectButtonLink text="백엔드" href="#"></MainSelectButtonLink>
+      <MainSelectButtonLink
+        text="전체보기"
+        href="/project_list"
+      ></MainSelectButtonLink>
+      <MainSelectButtonLink
+        text="기획"
+        href="/project_list"
+      ></MainSelectButtonLink>
+      <MainSelectButtonLink
+        text="UX/UI디자인"
+        href="/project_list"
+      ></MainSelectButtonLink>
+      <MainSelectButtonLink
+        text="퍼블리싱"
+        href="/project_list"
+      ></MainSelectButtonLink>
+      <MainSelectButtonLink
+        text="프론트엔드"
+        href="/project_list"
+      ></MainSelectButtonLink>
+      <MainSelectButtonLink
+        text="백엔드"
+        href="/project_list"
+      ></MainSelectButtonLink>
     </div>
   );
 }
--- src/components/organism/HeaderBottomWrap.tsx
+++ src/components/organism/HeaderBottomWrap.tsx
@@ -6,9 +6,9 @@ export default function HeaderBottomWrap() {
   return (
     <div className="flex justify-between items-center">
       <ul className="flex gap-[12px]">
-        <NavLink text="프로젝트" href="#" />
+        <NavLink text="프로젝트" href="/project_list" />
         <NavLink text="멤버" href="#" />
-        <NavLink text="공간 예약" href="#" />
+        <NavLink text="공간 예약" href="/reservation" />
         <li className="text-[#c6c6d0] py-[8px] px-0">|</li>
         <NavLink text="커뮤니티" href="#" />
         <NavLink text="완성 프로젝트" href="#" />
