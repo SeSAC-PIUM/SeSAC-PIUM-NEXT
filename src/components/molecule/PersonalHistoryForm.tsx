7401d6d21fcb239dd948fc447be6ad49d0ff77e4
Dayglow0926 <dayglow0926@gmail.com>
2023-10-13 10:00:40 +0900

    feat: register 추가

--- src/components/molecule/PersonalHistoryForm.tsx
+++ src/components/molecule/PersonalHistoryForm.tsx
@@ -2,18 +2,24 @@ import React, { useState } from "react";
 import SignUpTitleBox from "./SignUpTitleBox";
 import ChipButtonName from "../atoms/button/ChipButtonNameProps";
 import DefaultCard from "../atoms/card/DefaultCard";
+import { UseFormRegisterReturn } from "react-hook-form";
 
 interface PersonalHistoryProps {
   className?: string;
+  register: UseFormRegisterReturn;
 }
 
-export default function PersonalHistory({ className }: PersonalHistoryProps) {
+export default function PersonalHistory({
+  className,
+  register,
+}: PersonalHistoryProps) {
   const [isClick, setIsClick] = useState("비공개");
   return (
     <div className={className}>
       <DefaultCard addClassName="flex items-center gap-5">
         <div className="grid gap-x-4 gap-y-4 grid-cols-4">
           <ChipButtonName
+            register={register}
             label="학생"
             name="PersonalHistory"
             value="학생"
@@ -21,6 +27,7 @@ export default function PersonalHistory({ className }: PersonalHistoryProps) {
             onClick={() => setIsClick("학생")}
           />
           <ChipButtonName
+            register={register}
             label="직장"
             name="PersonalHistory"
             value="직장"
@@ -28,6 +35,7 @@ export default function PersonalHistory({ className }: PersonalHistoryProps) {
             onClick={() => setIsClick("직장")}
           />
           <ChipButtonName
+            register={register}
             label="창업"
             name="PersonalHistory"
             value="창업"
@@ -35,6 +43,7 @@ export default function PersonalHistory({ className }: PersonalHistoryProps) {
             onClick={() => setIsClick("창업")}
           />
           <ChipButtonName
+            register={register}
             label="비공개"
             name="PersonalHistory"
             value="비공개"
--- src/components/organism/SignUpContentHistory.tsx
+++ src/components/organism/SignUpContentHistory.tsx
@@ -3,24 +3,39 @@ import SignUpTitleBox from "../molecule/SignUpTitleBox";
 import DefaultInput from "../atoms/input/DefaultInput";
 
 import PersonalHistoryForm from "../molecule/PersonalHistoryForm";
+import { UseFormRegister } from "react-hook-form";
 
 interface SignUpContentHistoryProps {
   className: string;
+  register: UseFormRegister<any>;
 }
 
 export default function SignUpContentHistory({
   className,
+  register,
 }: SignUpContentHistoryProps) {
   return (
     <div className={className}>
       <SignUpTitleBox title="이런일을 했었어요!" />
 
       <div className="flex flex-col gap-3">
-        <PersonalHistoryForm />
-        <DefaultInput placeholder="전공, 취/창업 분야" />
+        <PersonalHistoryForm register={register("history")} />
+        <DefaultInput
+          register={register("field")}
+          placeholder="전공, 취/창업 분야"
+          autoComplete="autoComplete"
+        />
         <div className="flex gap-3">
-          <DefaultInput placeholder="링크 명칭(ex. 포트폴리오, github)" />
-          <DefaultInput placeholder="개인 사이트, 깃허브, 포트폴리오, URL" />
+          <DefaultInput
+            register={register("link")}
+            autoComplete="autoComplete"
+            placeholder="링크 명칭(ex. 포트폴리오, github)"
+          />
+          <DefaultInput
+            register={register("url")}
+            autoComplete="autoComplete"
+            placeholder="개인 사이트, 깃허브, 포트폴리오, URL"
+          />
         </div>
       </div>
     </div>
