167567722194243eb4f08b66bd6e916ff434f0b6
Dayglow0926 <dayglow0926@gmail.com>
2023-10-13 10:04:58 +0900

    feat: mbti 기능 추가

--- src/components/organism/SignUpContentMBTI2.tsx
+++ src/components/organism/SignUpContentMBTI2.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SignUpTitleBox from "../molecule/SignUpTitleBox";
 import ChipButtonName from "../atoms/button/ChipButtonNameProps";
 import DefaultCard from "../atoms/card/DefaultCard";
+import { UseFormSetValue } from "react-hook-form";
 
 interface SignUpContentMbti2Props {
   className?: string;
+  setValue: UseFormSetValue<any>;
 }
 
 export default function SignUpContentMbti2({
   className,
+  setValue,
 }: SignUpContentMbti2Props) {
-  const [isClick, setIsClick] = useState("비공개");
+  const [isClick, setIsClick] = useState<string[]>(["비공개"]);
+
+  useEffect(() => {
+    const mbti = isClick.join("");
+    if (mbti.length === 4 || mbti === "비공개") {
+      setValue("mbti", mbti);
+    }
+  }, [isClick]);
+
   return (
     <div className={className}>
       <SignUpTitleBox title="MBTI" />
@@ -21,57 +32,73 @@ export default function SignUpContentMbti2({
             label="I"
             name="IE"
             value="I"
-            clicked={isClick}
-            onClick={() => setIsClick("I")}
+            clicked={isClick[0]}
+            onClick={() =>
+              setIsClick((prev) => ["I", prev[1], prev[2], prev[3]])
+            }
           />
           <ChipButtonName
             label="S"
             name="SN"
             value="S"
-            clicked={isClick}
-            onClick={() => setIsClick("S")}
+            clicked={isClick[1]}
+            onClick={() =>
+              setIsClick((prev) => [prev[0], "S", prev[2], prev[3]])
+            }
           />
           <ChipButtonName
             label="F"
             name="FT"
             value="F"
-            clicked={isClick}
-            onClick={() => setIsClick("F")}
+            clicked={isClick[2]}
+            onClick={() =>
+              setIsClick((prev) => [prev[0], prev[1], "F", prev[3]])
+            }
           />
           <ChipButtonName
             label="P"
             name="PJ"
             value="P"
-            clicked={isClick}
-            onClick={() => setIsClick("P")}
+            clicked={isClick[3]}
+            onClick={() =>
+              setIsClick((prev) => [prev[0], prev[1], prev[2], "P"])
+            }
           />
           <ChipButtonName
             label="E"
             name="IE"
             value="E"
-            clicked={isClick}
-            onClick={() => setIsClick("E")}
+            clicked={isClick[0]}
+            onClick={() =>
+              setIsClick((prev) => ["E", prev[1], prev[2], prev[3]])
+            }
           />
           <ChipButtonName
             label="N"
             name="SN"
             value="N"
-            clicked={isClick}
-            onClick={() => setIsClick("N")}
+            clicked={isClick[1]}
+            onClick={() =>
+              setIsClick((prev) => [prev[0], "N", prev[2], prev[3]])
+            }
           />
           <ChipButtonName
             label="T"
             name="FT"
             value="T"
-            clicked={isClick}
-            onClick={() => setIsClick("T")}
+            clicked={isClick[2]}
+            onClick={() =>
+              setIsClick((prev) => [prev[0], prev[1], "T", prev[3]])
+            }
           />
           <ChipButtonName
             label="J"
             name="PJ"
             value="J"
-            clicked={isClick}
-            onClick={() => setIsClick("J")}
+            clicked={isClick[3]}
+            onClick={() =>
+              setIsClick((prev) => [prev[0], prev[1], prev[2], "J"])
+            }
           />
         </div>
         <ChipButtonName
@@ -79,8 +106,8 @@ export default function SignUpContentMbti2({
           name="secrete"
           value="비공개"
           checked={true}
-          clicked={isClick}
-          onClick={() => setIsClick("비공개")}
+          clicked={isClick.length === 1 ? "비공개" : ""}
+          onClick={() => setIsClick(["비공개"])}
         />
       </DefaultCard>
     </div>
