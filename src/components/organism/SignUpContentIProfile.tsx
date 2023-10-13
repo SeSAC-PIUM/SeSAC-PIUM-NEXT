9ce90d7877e990b7c3f77254a2b7742f17ce7fce
Dayglow0926 <dayglow0926@gmail.com>
2023-10-13 10:04:33 +0900

    feat: profile 함수 추가

--- src/components/organism/SignUpContentIProfile.tsx
+++ src/components/organism/SignUpContentIProfile.tsx
@@ -5,17 +5,19 @@ import GreenBorderButton from "../atoms/button/GreenBorderButton";
 
 interface SignUpContentIProfileProps {
   className?: string;
+  onFileChange?: any;
 }
 
 export default function SignUpContentIProfile({
   className,
+  onFileChange,
 }: SignUpContentIProfileProps) {
   return (
     <>
       <div className={className}>
         <SignUpTitleBox title="프로필 사진" />
         <div className="flex flex-wrap ">
-          <FileUpload />
+          <FileUpload onFileChange={onFileChange} />
           <div className="flex flex-col justify-center gap-3 my-4"></div>
         </div>
       </div>
