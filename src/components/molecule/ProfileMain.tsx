29e69d867f47d87b8ce64ef1444728a286150827
lkf6214 <lkf6214@naver.com>
2023-10-13 14:42:55 +0900

    add : profile gap

--- src/components/molecule/ProfileMain.tsx
+++ src/components/molecule/ProfileMain.tsx
@@ -9,47 +9,39 @@ import basicInfo from "@/data/basicInfo";
 import Image from "next/image";
 
 interface ProfileMainProps {
-  data: basicInfo;
+	data: basicInfo;
 }
 
 export default function ProfileMain({ data }: ProfileMainProps) {
-  return (
-    <>
-      <div className="flex justify-between">
-        <div className="flex gap-10">
-          <div className="bg-[--color-grayscale-100] w-[180px] h-[180px] rounded-full relative">
-            <Image src={data.profile} layout="fill" alt="profile" />
-          </div>
-          <div className="flex flex-col gap-8 justify-center w-[260px] ">
-            <div>
-              <LargeLabel title={data.name} />
-              <DefaultLabel title={data.job} />
-            </div>
+	return (
+		<>
+			<div className="flex justify-between">
+				<div className="flex gap-10">
+					<div className="bg-[--color-grayscale-100] w-[180px] h-[180px] rounded-full relative">
+						<Image src={data.profile} layout="fill" alt="profile" />
+					</div>
+					<div className="flex flex-col gap-8 justify-center w-[260px] ">
+						<div>
+							<LargeLabel title={data.name} />
+							<DefaultLabel title={data.job} />
+						</div>
 
-            <DescriptionLabel title={data.introduce} />
-            <div className="">
-              <GreenCard
-                text={`${
-                  data.mentoring === "mentor"
-                    ? "멘토"
-                    : data.mentoring === "mentee"
-                    ? "멘티"
-                    : ""
-                }`}
-              />
-              <GreenCard text={data.mbti} />
-            </div>
-          </div>
-        </div>
+						<DescriptionLabel title={data.introduce} />
+						<div className="flex gap-2">
+							<GreenCard text={`${data.mentoring === "mentor" ? "멘토" : data.mentoring === "mentee" ? "멘티" : ""}`} />
+							<GreenCard text={data.mbti} />
+						</div>
+					</div>
+				</div>
 
-        {/* DefaultButton 사용보다 Link 사용이 더 좋을 것 같아서 변경했습니다. */}
-        <Link
-          className="border-[1px] border-[--color-grayscale-200] rounded w-fit py-2 px-4 flex items-center "
-          href="/signUp_001_Basic"
-        >
-          프로필 수정하기
-        </Link>
-      </div>
-    </>
-  );
+				{/* DefaultButton 사용보다 Link 사용이 더 좋을 것 같아서 변경했습니다. */}
+				<Link
+					className="border-[1px] border-[--color-grayscale-200] rounded w-fit py-2 px-4 flex items-center "
+					href="/signUp_001_Basic"
+				>
+					프로필 수정하기
+				</Link>
+			</div>
+		</>
+	);
 }
